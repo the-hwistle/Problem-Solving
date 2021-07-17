c0b349d1470e535cd7ee34c60fae13ab905742c1
HuiSe <42146731+Beatriz-Yun@users.noreply.github.com>
2021-07-17 22:34:23 +0900

    Update 숫자문자열과영단어.py

--- 프로그래머스/숫자문자열과영단어.py
+++ 프로그래머스/숫자문자열과영단어.py
@@ -1,5 +1,5 @@
 def solution(s):
-    eng = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    eng = {'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
     
     answer=''    # 리턴값
     word = ''    # 임시버퍼
@@ -10,11 +10,12 @@ def solution(s):
             if word:
                 answer += eng[word]  # 버퍼에 있는 문자열을 숫자로 변환후 리턴값에 추가
                 word = ''            # 버퍼 초기화
-            answer+=char    # 숫자는 바로 리턴값에 추가
+            answer+=char             # 숫자는 바로 리턴값(answer)에 추가
         else:
-            if word in eng.keys():
-                answer += eng[word]
-                word = ''
+            if len(word)>=3:         # 딕셔너리eng의 키는 최소 문자가 3개 이상이어야 한다.
+                if word in eng.keys():
+                    answer += eng[word]
+                    word = ''
             word += char
         # print(answer)
         # print('word:', word)
