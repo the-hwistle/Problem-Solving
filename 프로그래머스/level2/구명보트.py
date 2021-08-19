404221f58c4a3dd5b5139636fa356d4f473437bf
HuiSe <42146731+Beatriz-Yun@users.noreply.github.com>
2021-08-20 01:30:02 +0900

    탐욕법(Greedy)

--- 프로그래머스/level2/구명보트.py
+++ 프로그래머스/level2/구명보트.py
@@ -33,3 +33,30 @@ def solution(people, limit):
         answer += 1
         
     return answer
+
+
+# 풀이2) 정확성 통과. 효율성 통과 (max, min대신 인덱싱만 조절)
+
+# 입력: 몸무게를 담은 배열, 무게제한
+# 출력: 필요한 구명보트 최소 개수
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+    first = 0
+    last = len(people)-1
+    
+    while first < last:
+        if people[first] + people[last] > limit:
+            last -= 1
+            answer += 1
+            
+        else:
+            first += 1
+            last -= 1
+            answer += 1
+    
+    if first == last:
+        answer += 1
+        
+    return answer
