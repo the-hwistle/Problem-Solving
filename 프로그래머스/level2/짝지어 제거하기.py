e4dd216807a8c490beef710aa9ccc07af0f1ab11
HuiSe <42146731+Beatriz-Yun@users.noreply.github.com>
2021-08-28 23:24:12 +0900

    2017 팁스타운

---
+++ 프로그래머스/level2/짝지어 제거하기.py
@@ -0,0 +1,9 @@
+# 프로그래머스 > 코딩테스트 연습 > 2017 팁스타운 > 문제: 짝지어 제거하기
+# 문제링크: https://programmers.co.kr/learn/courses/30/lessons/12973
+
+def solution(s):
+    answer = -1
+    
+    print(s)
+
+    return answer
