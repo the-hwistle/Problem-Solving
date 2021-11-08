f86f74370dbf911cdce145d4c4db88713f8dc1b8
HuiSe <42146731+Beatriz-Yun@users.noreply.github.com>
2021-11-09 00:49:35 +0900

    [해결] DFS

--- 백준/2606_바이러스.py
+++ 백준/2606_바이러스.py
@@ -5,22 +5,28 @@
 # 입력2: N = 네트워크 상에서 직접 연결되어 있는 컴퓨터 쌍의 수
 # 입력3: N만큼 한 줄에 한 쌍씩 네트워크 상에서 직접 연결되어 있는 컴퓨터의 번호 쌍
 
+count = 0
 def dfs(graph, v, visited):
+  global count
   visited[v] = True
-  print(v, end=' ')
+  #print(v, end=' ')
   for i in graph[v]:
     if not visited[i]:
       dfs(graph, i, visited)
+      count += 1
 
 
 M = int(input())
 N = int(input())
-graph = [[]]
+graph = [[] for _ in range(M+1)]
 for i in range(N):
-  temp = list(map(int, input().split()))
-  graph.append(temp)
+  a, b = list(map(int, input().split()))
+  graph[a].append(b)
+  graph[b].append(a)
 
-print(graph)
+#print(graph)
 
 visited = [False]*(M+1)
 dfs(graph, 1, visited)
+#print(visited)
+print(count)
